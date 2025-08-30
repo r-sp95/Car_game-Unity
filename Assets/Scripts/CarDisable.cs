eb19b837fe2a828a27fe334d3104772a1497d031
r-sp95 <muhammadtaha3950@gmail.com>
2025-08-30 15:41:40 +0500

    The First Commit

---
+++ Assets/Scripts/CarDisable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDisable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Car = GameObject.Find("FamilyCar");
+        Rigidbody body = Car.GetComponent<Rigidbody>();
+        body.isKinematic = true;
+
+        print(Car);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text myText = null;
+    private float TimerElapsed = 0.00f; // a floating point value to store the elapsed time
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        myText = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerElapsed += Time.deltaTime;
+        myText.text = TimerElapsed.ToString("00");
+    }
+}
